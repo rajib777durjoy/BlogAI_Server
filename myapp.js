897a27f81a29880dcfc89b5b89aee748974641f2
rajib777durjoy <rajib777chanda@gmail.com>
2025-06-11 03:21:40 +0600

    Gemini AI for Text generation done

--- myapp.js
+++ myapp.js
@@ -1,8 +1,11 @@
+
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
-const cors= require('cors');
+const cors = require('cors');
+const { GoogleGenAI } = require('@google/genai');
+
 
 // app.use(cors({
 //     origin:['http://localhost:3000'],
@@ -11,12 +14,9 @@ const cors= require('cors');
 app.use(cors())
 app.use(express.json());
 
-// A sample route
-
-
-
+// A sample route;
 const uri = `mongodb+srv://${process.env.userDB}:${process.env.PasswordDb}@cluster0.u87dt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log('mongodb connection',uri)
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -31,22 +31,41 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-     // await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     //--------------------Database Collection-------------------------------------------------//
     const database = client.db('Blog_Platform');
-    const postCollection=database.collection('post_list');
+    const postCollection = database.collection('post_list');
 
 
-//---------------------------------- API functionality Start Here ------------------------------//
-     app.post('/blogPost/:user_id',async(req,res)=>{
-      const userId= req.params?.user_id;
-      const Info= req.body;
-      const post= await postCollection.insertOne(Info);
-      console.log('blogInfo',post)
+    //---------------------------------- API functionality Start Here ------------------------------//
+    app.post('/blogPost/:user_id', async (req, res) => {
+      const userId = req.params?.user_id;
+      const Info = req.body;
+      if (!Info.title || !Info.description || !Info.image) {
+        return res.status(400).send({ error: "Missing required fields" });
+      }
+      const post = await postCollection.insertOne(Info);
+      console.log('blogInfo', post)
       res.send(post)
-     })
+    })
+
+    //-------------------------------------------Gemini text generate--------------------//
+    const ai = new GoogleGenAI({ apiKey: process.env.Gemini_API_KEY });
+    //  console.log(ai)
+    app.get('/Gemini/textGenarate', async (req, res) => {
+      const text = req?.query?.text;
+      const textGenarate = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: text,
+      });
+      const cleanText = (textGenarate.text)
+        .replace(/\*\*/g, '')
+        .replace(/\*/g, '')
+        .trim();
+      res.send({ result: cleanText })
+    })
 
   } finally {
     // Ensures that the client will close when you finish/error
@@ -60,4 +79,4 @@ app.get('/', (req, res) => {
 
 
 
-module.exports=app;
+module.exports = app;
