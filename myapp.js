4cf1dd3dadb676b62766298f9a1fc6a625a3a407
rajib777durjoy <rajib777chanda@gmail.com>
2025-06-13 02:40:52 +0600

    Text and Image generate use to Gemini AI

--- myapp.js
+++ myapp.js
@@ -4,19 +4,24 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const cors = require('cors');
-const { GoogleGenAI } = require('@google/genai');
+const { GoogleGenAI, Modality } = require('@google/genai');
+const Axios = require('axios');
+const FormData = require('form-data');
 
 
 // app.use(cors({
 //     origin:['http://localhost:3000'],
 //     credentials: true
 // }));
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 // A sample route;
 const uri = `mongodb+srv://${process.env.userDB}:${process.env.PasswordDb}@cluster0.u87dt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+//-------------------------------ImgBb ApI key --------------------------------------------------//
+
+const Image_API_KEY = `https://api.imgbb.com/1/upload?key=${process.env.ImgBB_API_KEY}`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -66,7 +71,39 @@ async function run() {
         .trim();
       res.send({ result: cleanText })
     })
+    //------------------------------Gemini Image Generate-----------------------------------//
+    app.get('/Gemini/ImageGenerate', async (req, res) => {
+      console.log('hello world Image Generate functions');
+      const ImgText=req.query.imgText;
 
+      const ai = new GoogleGenAI({ apiKey: process.env.Gemini_API_KEY });
+      const contents =
+        "Hi, can you create a 3d rendered image " + ImgText; 
+
+      // Set responseModalities to include "Image" so the model can generate  an image
+      const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash-preview-image-generation",
+        contents: contents,
+        config: {
+          responseModalities: [Modality.TEXT, Modality.IMAGE],
+        },
+      });
+      for (const part of response.candidates[0].content.parts) {
+        // Based on the part type, either show the text or save the image
+        if (part.text) {
+          // console.log(part.text);
+        } else if (part.inlineData) {
+
+          const imageData = part.inlineData.data;
+          // const buffer = Buffer.from(imageData, "base64");
+          // const base64Image = buffer.toString("base64");
+          const buffer = Buffer.from(imageData, 'base64');
+          res.set('Content-Type', 'image/png');
+          res.send(buffer);
+        }
+      }
+
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
