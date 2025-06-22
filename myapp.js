7483dbc38483f2e1e2cd14f4658cd6f74f649d5b
rajib777durjoy <rajib777chanda@gmail.com>
2025-06-23 01:55:37 +0600

    cors issuce

--- myapp.js
+++ myapp.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const { GoogleGenAI, Modality } = require('@google/genai');
 const imagekit = require('./imageKit');
 app.use(cors({
-  origin:'https://blog-p9bv8uj8p-durjoychandos-projects.vercel.app',
+  origin:['https://blog-p9bv8uj8p-durjoychandos-projects.vercel.app','https://blog-pirvay6t3-durjoychandos-projects.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
